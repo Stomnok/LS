3eb969cb380ae7fe1e86375ebbdc30746e7c1249
Ivan White <51106782+Stomnok@users.noreply.github.com>
2021-06-12 15:40:03 +0700

    Latin Square is a class now

--- LS.cpp
+++ LS.cpp
@@ -5,108 +5,124 @@
 using namespace std;
 typedef unsigned int UI;
 
-struct LS {
-    int **pointer = nullptr;
-    int order = 0;
-    int orderBasic = 0;
+class LatinSquare {
+public:
+    int **pointer;
+    int order;
+    int orderBasic;
+    LatinSquare();
+    LatinSquare(int order, int orderBasic);
+    ~LatinSquare();
+    void Print() const;
+    void MakeBasicLS() const;
+    void ShiftAlphabet() const;
+    void FillHalfLS(LatinSquare* A, LatinSquare* B, char UorD) const;
+
 };
 
 
-void Delete(const struct LS LS, const UI *Mask = nullptr) {
-    for (int p = 0; p < LS.order; p++) {
-        delete[]LS.pointer[p];
-    }
-    delete[]LS.pointer;
-    delete[]Mask;
+LatinSquare::LatinSquare(){
+    order = 0;
+    orderBasic = 0;
+    pointer = nullptr;
 }
 
+LatinSquare::LatinSquare(int _order, int _orderBasic){
+    order = _order;
+    orderBasic = _orderBasic;
+    pointer = new int *[order];
+    for (int i = 0; i < order; i++) {
+        pointer[i] = new int[order];
+        for (int j = 0; j < order; j++) {
+            pointer[i][j] = 0;
+        }
+    }
+}
 
-void Print(const struct LS LS) {   //toDo заменить вывод на запись в файл
-    for (int j = 0; j < LS.order; j++) {
-        for (int i = 0; i < LS.order; i++)
-            cout << LS.pointer[j][i] << " ";
+LatinSquare::~LatinSquare(){
+    for (int p = 0; p < order; p++) {
+        delete[]pointer[p];
+    }
+    delete[]pointer;
+}
+
+void LatinSquare::Print() const {   //toDo заменить вывод на запись в файл
+    for (int j = 0; j < order; j++) {
+        for (int i = 0; i < order; i++)
+            cout << pointer[j][i] << " ";
         cout << endl;
     }
+    cout.flush();
 }
 
-
-void test(struct LS LS, int g, int t) {
-    for (int i = 0; i < LS.order; i++) {
-        for (int j = 0; j < LS.order / 2; j++) {
-            LS.pointer[i][g] = g;
-        }
-    }
-    if (g % 2 != 0) {
-        return;
-    }
-    int gB = g - (LS.order / int(pow(2, t + 2)));
-    int gA = g + (LS.order / int(pow(2, t + 2)));
-    thread A(test, LS, gA, t + 1);
-    thread B(test, LS, gB, t + 1);
-    A.join();
-    B.join();
-}
-
-
-void MakeBasicLS(struct LS LS) {   //toDo
-    LS.pointer = new int *[LS.order];
-    for (int i = 0; i < LS.order; i++) {
-        LS.pointer[i] = new int[LS.order];
-        for (int j = 0; j < LS.order; j++) {
-            LS.pointer[i][j] = 0;
+void LatinSquare::MakeBasicLS() const {   //toDo
+    for (int i = 0; i < order; i++) {
+        for (int j = 0; j < order; j++) {
+            pointer[i][j] = (i + j) % order;
         }
     }
 }
 
+void LatinSquare::ShiftAlphabet() const {
+    for (int i = 0; i < order; i++) {
+        for (int j = 0; j < order; j++) {
+            pointer[i][j] += order;
+        }
+    }
+}
 
-void ShiftAlphabet(LS B) {  //toDo
+void LatinSquare::FillHalfLS(LatinSquare* A,LatinSquare* B, char UorD) const {
+    int shift = 0;
+    if (UorD == 'd') {
+        shift = A->order;
+    }
+    for (int i = 0; i < A->order; i++) {
+        for (int j = 0; j < A->order; j++) {
+            pointer[i + shift][j] = A->pointer[i][j];
+        }
+        for (int j = 0; j < A->order; j++) {
+            pointer[i + shift][j + A->order] = B->pointer[i][j];
+        }
+    }
+}
+
+
+void Permutation(LatinSquare* LS) { //toDo
 
 }
 
 
-void FillHalfLS(struct LS LS, const struct LS A, const struct LS B, char UorD) { //toDo
-
-}
-
-
-void Permutation(struct LS LS){ //toDo
-
-}
-
-
-void MakeLS(struct LS LS) {
-    if (LS.order == LS.orderBasic) {
-        MakeBasicLS(LS);
+void MakeLS(LatinSquare* LS) {
+    if (LS->order == LS->orderBasic) {
+        LS->MakeBasicLS();
         return;
     }
     //step 1
     //step 1.a
-    struct LS A;
-    A.order = (LS.order >> 1);
-    A.orderBasic = LS.orderBasic;
-    thread generateA(MakeLS, A);
+    LatinSquare A(LS->order >> 1,LS->orderBasic);
+    thread generateA(MakeLS,&A);
     //step 1.b
-    struct LS B;
-    B.order = (LS.order >> 1);
-    B.orderBasic = LS.orderBasic;
-    thread generateB(MakeLS, B);
+    LatinSquare B(LS->order >> 1,LS->orderBasic);
+    thread generateB(MakeLS,&B);
     generateA.join();
     generateB.join();
+    cout << "A pointer is" << A.pointer << endl;
+    cout << "B pointer is" << B.pointer << endl;
     //step 2
-    ShiftAlphabet(B);
+    B.ShiftAlphabet();
     //step 4
     //step 4.1
-    FillHalfLS(LS, A, B, 'u');
+    LS->FillHalfLS(&A, &B, 'u');
     //step 3
     //step 3.a
-    thread permutationA(Permutation, A);
+    thread permutationA(Permutation,&A);
     //step 3.b
-    thread permutationB(Permutation, B);
+    thread permutationB(Permutation,&B);
     permutationA.join();
     permutationB.join();
     //step 4
     //step 4.2
-    FillHalfLS(LS, A, B, 'd');
+    LS->FillHalfLS(&B, &A, 'd');
     //step 5
     Permutation(LS);
 }
@@ -114,27 +130,17 @@ void MakeLS(struct LS LS) {
 
 int main() {
     srand(time(nullptr));
-    LS LS{};
-
+    int order,orderBasic;
     cout << "Input square order, power of 2:" << endl;
-    cin >> LS.order;
-    LS.orderBasic = 8;
-    if ((LS.order & (LS.order - 1)) != 0) {
+    cin >> order;
+    orderBasic = 8;
+    if ((order & (order - 1)) != 0) {
         cout << "Incorrect data, set order to 1024" << endl;
-        LS.order = 1024;
+        order = 1024;
     }
     cout.flush();
-
-    LS.pointer = new int *[LS.order];
-    for (int i = 0; i < LS.order; i++) {
-        LS.pointer[i] = new int[LS.order];
-        for (int j = 0; j < LS.order; j++) {
-            LS.pointer[i][j] = 0;
-        }
-    }
-    //test(LS,(LS.order/2), 0);
-    MakeLS(LS);
-    Print(LS);
-    Delete(LS);
+    LatinSquare LS(order,orderBasic);
+    MakeLS(&LS);
+    LS.Print();
     return 0;
 }
